571d650373c80f33dad1af4243189c40a77d7fc6
bijo-at-688165709689 <bijo@cordovacloud.com>
2025-05-11 20:29:14 +0100

    final commit - all basic functionalities completed

--- app/src/main/java/com/example/lost_and_found/ui/screens/items/ItemsViewModel.kt
+++ app/src/main/java/com/example/lost_and_found/ui/screens/items/ItemsViewModel.kt
@@ -1,6 +1,8 @@
 package com.example.lost_and_found.ui.screens.items
 
 
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -12,6 +14,7 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import timber.log.Timber
+import java.util.Locale
 import javax.inject.Inject
 
 @HiltViewModel
@@ -27,8 +30,51 @@ constructor(private val repository: FireStoreService,
     var isloading = mutableStateOf(false)
     var error = mutableStateOf(Exception())
 
+    private val _selectedCategories = mutableStateListOf<String>() // Store selected categories as Strings
+    val selectedCategories: List<String> get() = _selectedCategories
+
+    // Filtered list state
+    private val _filteredItems = MutableStateFlow<List<LostItemModel>>(emptyList())
+    val filterItems: StateFlow<List<LostItemModel>> = _filteredItems.asStateFlow()
+
 
     init { getAllLostItems() }
+
+
+
+    fun filterItems() {
+        _filteredItems.value = if (_selectedCategories.isEmpty() || _selectedCategories.contains("ALL")) {
+            _lostItems.value // Show all if nothing or "ALL" selected
+        } else {
+            _lostItems.value.filter { item ->
+                _selectedCategories.any { selected ->
+                    item.category.equals(selected, ignoreCase = true)
+                }
+            }
+        }
+    }
+
+
+
+    fun toggleCategory(category: String) {
+        if (category.equals("ALL", ignoreCase = true)) {
+            _selectedCategories.clear()
+            _selectedCategories.add("ALL")
+        } else {
+            _selectedCategories.remove("ALL") // Unselect "ALL" if selecting something else
+            if (_selectedCategories.contains(category)) {
+                _selectedCategories.remove(category)
+            } else {
+                _selectedCategories.add(category)
+            }
+        }
+    }
+
+
+    fun clearCategories() {
+        _selectedCategories.clear()
+    }
+
 
     fun getAllLostItems() {
         viewModelScope.launch {
